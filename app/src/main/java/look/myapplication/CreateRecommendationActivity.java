e949bf1fcb6858dd12203bd0426f573ff7ca5e14
smacnak <smacnak@gmail.com>
2017-04-25 16:51:55 -0400

    Update CreateRecommendationActivity.java

--- app/src/main/java/look/myapplication/CreateRecommendationActivity.java
+++ app/src/main/java/look/myapplication/CreateRecommendationActivity.java
@@ -20,6 +20,7 @@ public class CreateRecommendationActivity extends AsyncTask<String, Void, String
 
     private Context context;
     private User user;
+    private String recipient;
 
     public CreateRecommendationActivity(Context context, User user) {
         this.context = context;
@@ -28,9 +29,8 @@ public class CreateRecommendationActivity extends AsyncTask<String, Void, String
 
     @Override
     protected String doInBackground(String... arg0) {
-        MainActivity mainActivity = (MainActivity) context;
         String userName = arg0[0];
-        String recipient = arg0[1];
+        recipient = arg0[1];
         String content = arg0[2];
         String data;
         String link;
@@ -74,19 +74,20 @@ public class CreateRecommendationActivity extends AsyncTask<String, Void, String
             result = bufferedReader.readLine();
             return result;
         } catch (Exception e) {
-            mainActivity.sendError(e.getMessage());
             return new String("Exception: " + e.getMessage());
         }
     }
 
     protected void onPostExecute(String result) {
-        MainActivity mainActivity = (MainActivity) context;
         String jsonStr = result;
         if (jsonStr != null) {
             try {
                 JSONObject jsonObj = new JSONObject(jsonStr);
                 String query_result = jsonObj.getString("query_result");
                 if (query_result.equals("SUCCESS")) {
+                    String token = jsonObj.getString("message");
+                    MainActivity mainActivity = (MainActivity)context;
+                    mainActivity.send(recipient, token);
                     Toast.makeText(context, "Recommendation sent.", Toast.LENGTH_SHORT).show();
                 } else if (query_result.equals("FAILURE")) {
                     Toast.makeText(context, "Failed to send recommendation.", Toast.LENGTH_SHORT).show();
@@ -95,7 +96,6 @@ public class CreateRecommendationActivity extends AsyncTask<String, Void, String
                 }
             } catch (JSONException e) {
                 e.printStackTrace();
-                mainActivity.sendError(e.getMessage());
                 Toast.makeText(context, result , Toast.LENGTH_SHORT).show();
             }
         } else {
